09e1c11a3dc9422cc94352d1dfa3894aef823a63
Sayantansen123 <riosayan123@gmail.com>
2024-11-08 00:36:45 +0530

    cart route created

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
-import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser";
+import cartRoutes from "./routes/cart.route.js"
 
 //enviorment variable setup
 dotenv.config();
@@ -19,6 +20,7 @@ app.use(cookieParser())
 //routes setup
 app.use("/api/auth",authRoutes)
 app.use("/api/products",productRoutes)
+app.use("/api/cart",cartRoutes)
 
 app.listen(PORT,()=>{
     console.log("server is running on port http://localhost:" + PORT);
