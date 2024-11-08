041eeab666620e5c58c44d7c642a7d50ba4384c9
Sayantansen123 <riosayan123@gmail.com>
2024-11-08 18:17:14 +0530

    analytics route done

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js"
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
+import couponRoutes from "./routes/coupon.route.js"
+import analyticsRoutes from "./routes/analytics.routes.js"
 
 //enviorment variable setup
 dotenv.config();
@@ -21,6 +23,8 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/products",productRoutes)
 app.use("/api/cart",cartRoutes)
+app.use("/api/coupons",couponRoutes)
+app.use("/api/analytics",analyticsRoutes)
 
 app.listen(PORT,()=>{
     console.log("server is running on port http://localhost:" + PORT);
