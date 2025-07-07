457a34136d993bd574e79840c338a92fe1aefa44
Sayantansen123 <riosayan123@gmail.com>
2025-07-07 11:33:21 +0530

    get cart items

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,10 +1,7 @@
-import Redis from "ioredis"
-import dotenv from "dotenv"
+import Redis from "ioredis";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 //connecting to the redis upstach client
 export const redis = new Redis(process.env.UPSTACH_REDIS_URL);
-
-
-
